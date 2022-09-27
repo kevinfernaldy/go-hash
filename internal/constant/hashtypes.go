0f0527672c8db2c4fa620887996886e854fd376d
Kevin Fernaldy <kevin.fernaldy@gmail.com>
2022-09-27 22:53:11 +0700

    Added basic console for hash

--- internal/constant/hashtypes.go
+++ internal/constant/hashtypes.go
@@ -1,5 +1,10 @@
 package constant
 
+type HashType struct {
+	ID   int
+	Name string
+}
+
 const (
 	HashMD5 int = iota
 	HashSHA224
@@ -7,3 +12,11 @@ const (
 	HashSHA384
 	HashSHA512
 )
+
+var HashTypeList = []HashType{
+	{HashMD5, "MD5"},
+	{HashSHA224, "SHA224"},
+	{HashSHA256, "SHA256"},
+	{HashSHA384, "SHA384"},
+	{HashSHA512, "SHA512"},
+}
